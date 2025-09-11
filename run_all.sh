aaa1ccd56ebc451c279ed68adc68896c9ea513ab
kino-works <realkinomail@gmail.com>
2025-09-11 15:40:15 +0900

    self_correct 확인 완료

--- run_all.sh
+++ run_all.sh
@@ -3,8 +3,8 @@ set -e
 #python util/isr_llm_util/generate_household_cases.py --num_cases 200 --out_dir ./test_scenario
 echo "1. Run ISR-LLM"
 python baseline/isr_llm.py
-#echo "2. Run Self_correct"
-#python baseline/self_correct.py
+echo "2. Run Self_correct"
+python baseline/self_correct.py
 #echo "3. Run Cap"
 #python baseline/cap.py
 #echo "4. Run Delta"
